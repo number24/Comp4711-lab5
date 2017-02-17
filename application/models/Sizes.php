db85c233fc80c608ee4d83074684d1fcbda87b8c
jasonlearn <jasonchan521@hotmail.com>
2017-02-16 17:21:26 -0800

    Added model Flags, Groups, Priorities, Sizes, Statuses, Tasks

---
+++ application/models/Sizes.php
@@ -0,0 +1,8 @@
+<?php
+class Sizes extends MY_Model {
+        public function __construct()
+        {
+                parent::__construct('sizes', 'id');
+        }
+}
+?>
---
+++ application/models/Tasks.php
@@ -0,0 +1,8 @@
+<?php
+class Tasks extends MY_Model {
+        public function __construct()
+        {
+                parent::__construct('tasks', 'id');
+        }
+}
+?>
